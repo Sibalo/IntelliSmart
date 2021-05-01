b7c8575a5454f62bb7c74f96e427ccc11c311dce
Sibalo <mrsicelondlovu@gmail.com>
2021-05-01 22:39:05 +0200

    Links Debugging

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(() => {
+
+    $(".solut").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#solut").offset().top},
+            'fast');
+    });
+    $(".prods").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#products").offset().top},
+            'fast');
+    });
+    $("nav a").click(function(event) {
+        event.preventDefault();
+        switch ($(this).text()){
+            case ("About"):
+                $('html,body').animate({
+                    scrollTop: $("#about").offset().top},
+                    'fast');
+                break;
+            case ("Products & Solutions"):
+                $('html,body').animate({
+                    scrollTop: $("#products").offset().top},
+                    'fast');
+                break;
+            default:
+                $('html,body').animate({
+                    scrollTop: $("#contact").offset().top},
+                    'fast');
+        }
+
+    });
     
 })
